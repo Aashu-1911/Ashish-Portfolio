2cb72ac4c4cf7be112205a1f9d9e4f4fa2ea9e88
Aashu-1911 <ashishbiradar.1911@gmail.com>
2026-06-17 17:03:44 +0530

    fix api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || "/api",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default api;
-
-
-
-
 const api = axios.create({
   baseURL: "https://ashish-biradar-portfolio.onrender.com/api",
   headers: {
