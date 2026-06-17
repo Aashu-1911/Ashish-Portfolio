a1efcf0bd0648288c07710288775ef5ffe7ee503
Aashu-1911 <ashishbiradar.1911@gmail.com>
2026-06-17 16:56:21 +0530

    api error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,9 +10,13 @@ import axios from "axios";
 // export default api;
 
 
+
+
 const api = axios.create({
   baseURL: "https://ashish-biradar-portfolio.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+export default api;
