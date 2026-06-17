234ed37b1a30159d9023e8cbe2798c4f7d617131
Aashu-1911 <ashishbiradar.1911@gmail.com>
2026-06-17 17:48:15 +0530

    add request logging

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 app.use(
   "/media/projects_images",
   express.static(path.join(__dirname, "uploads", "projects_images"))
