b7866543c779c130986dfaa2956085adc02c9eaa
Aashu-1911 <ashishbiradar.1911@gmail.com>
2026-06-30 20:07:00 +0530

    updated resume link

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({id}) => {
   return (
     <nav className="navbar" id={id}>
       <div className="navbar-right">
-        <a href="https://drive.google.com/drive/u/0/folders/1rgLQWcYpr2sDJSlRT-3fKMaFb_xABLtu/view?usp=sharing" className="navbar-connect" target="blank_">
+        <a href="https://drive.google.com/drive/u/0/folders/1rgLQWcYpr2sDJSlRT-3fKMaFb_xABLtu" className="navbar-connect" target="blank_">
           <span className="connect-border">Resume</span>
         </a>
       </div>
